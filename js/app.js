aef41d5764fd1d9f08306f62084c3fae9da22151
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-06 22:30:37 +0100

    ADD: function that generates only HTML of the cards

--- js/app.js
+++ js/app.js
@@ -3,30 +3,49 @@ rowEl = document.querySelector(".row");
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     .then((res) => {
         const posts = res.data;
-        appendPosts(posts, rowEl);
+        appendPosts(posts);
     })
     .catch((err) => {
-        console.err(err);
+        console.error(err);
     });
 
-function appendPosts(posts, root) {
+
+function appendPosts(posts) {
     console.log(posts);
 
     posts.forEach((post) => {
         let { url, title } = post
         title = title.charAt(0).toUpperCase() + title.slice(1);
-        
-        const postCard = `
-        <div class="col-md-6 col-lg-4 mb-4">
-            <div class="card p-3 rounded-0 h-100">
-                <img src="./img/pin.svg" class="pin">
-                <img src="${url}" class="card-img-top">
-                <div class="card-body">
-                        <p class="card-text">${title}</p>
-                </div>
-            </div>
-        </div>`
 
-        root.innerHTML += postCard
+        generateCards(url, title);
+        
+        // const postCard = `
+        // <div class="col-md-6 col-lg-4 mb-4">
+        //     <div class="card p-3 rounded-0 h-100">
+        //         <img src="./img/pin.svg" class="pin">
+        //         <img src="${url}" class="card-img-top">
+        //         <div class="card-body">
+        //                 <p class="card-text">${title}</p>
+        //         </div>
+        //     </div>
+        // </div>`
+
+        // root.innerHTML += postCard
     });
 };
+
+
+function generateCards (surce, description) {
+    const postCard = `
+    <div class="col-md-6 col-lg-4 mb-4">
+        <div class="card p-3 rounded-0 h-100">
+            <img src="./img/pin.svg" class="pin">
+            <img src="${surce}" class="card-img-top">
+            <div class="card-body">
+                    <p class="card-text">${description}</p>
+            </div>
+        </div>
+    </div>`
+
+    rowEl.innerHTML += postCard;
+}
